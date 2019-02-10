24efa57f553b0061d5358c5a0afbe439f64550e0
Anthony Ellis <AEllisNOLA@gmail.com>
2019-02-10 15:54:13 -0600

    Checkpoint 2750 completed. Created ToDoItem component which is rendering to screen.

---
+++ src/components/ToDoItem.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class ToDoItem extends Component {
+    render() {
+        return (
+            <div>
+                <li>A ToDoItem will go here.</li>
+            </div>
+        )
+    }
+}
+
+
+export default ToDoItem;
